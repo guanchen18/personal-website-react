bc92a524cfa00ff8b232f5def79eb852fcd9b09f
Guanchen <guanchen.song.797@gmail.com>
2020-02-19 12:24:02 +0100

    Create Footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Work from './components/Work';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <div className="App-header"><Header /></div>
       <div className="About"><About /></div>
       <div className="work"><Work /></div>
-      <div className="contact"><Contact /></div>
+      <div className="contact"><Contact /> <Footer /></div>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component{
+    state = {}
+
+    render () {
+        return (
+            <div>
+                <span className="footer">Made ❤ by Guanchen Song</span>
+            </div>
+        )
+    }
+}
+
+export default Footer;
